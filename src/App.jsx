d7712c86de3fef76da9e7084f30eb6efd7fc8776
SunnyJate <sunnyjate2007@gmail.com>
2024-06-20 17:15:07 +0530

    ok

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Components/Header"
+import Body from "./Components/Body"
+import Footer from "./Components/Footer"
+
+
+function App() {
+   return (
+    <>
+      <div className="bg-indigo-500 px-6 py-3 min-h-screen flex flex-col gap-16">
+        <Header/>
+        <Body/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
