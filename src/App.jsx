79c655d3b639cf46e9420c715ede960175dfec5e
SunnyJate <sunnyjate2007@gmail.com>
2024-06-22 12:42:28 +0530

    final project

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Footer from "./Components/Footer"
 
 
 function App() {
-   return (
+
+  return (
     <>
-      <div className="bg-indigo-500 px-6 py-3 min-h-screen flex flex-col gap-16">
-        <Header/>
-        <Body/>
-        <Footer/>
+      <div className="bg-yellow-100 py-3 px-6 flex flex-col min-h- min-h-screen gap-16 md:px-10 md:py-7 md:justify-between lg:px-16 lg:py-10">
+      <Header/>
+      <Body/>
+      <Footer/>
       </div>
+      
     </>
   )
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex flex-col gap-12 md:flex-row md:justify-between'>
+      <ul className='flex gap-6 font text-gray-500'>
+        <li>
+          <a href="#">Facebook</a>
+        </li>
+        <li>
+          <a href="#">Instagram</a>
+        </li>
+        <li>
+          <a href="#">Twitter</a>
+        </li>
+      </ul>
+      <div className='flex gap-2'>
+        <img src="./assets/Help-Avatar.svg" alt="Help" />
+        <div>
+          <p className='font-playfair font-thin'>Have any Questions?</p>
+          <a href="#" className='font-lato font-medium'>Talk To a Specialist.</a>
+        </div>      </div>
+    </div>
   )
 }
-
 export default Footer
